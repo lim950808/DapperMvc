805c1fb75bc28b4d41c63364f1af428aa3ce2732
Srook <Srook@DESKTOP-5HRMSRB>
2022-06-20 10:16:47 +0900

    초기 커밋.

---
+++ DapperMvc/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using DapperMvc.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using Dapper;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DapperMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Friend  
+        public ActionResult Index()
+        {
+            List<Friend> FriendList = new List<Friend>();
+            using (IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["friendConnection"].ConnectionString))
+            {
+
+                FriendList = db.Query<Friend>("Select * From tblFriends").ToList();
+            }
+            return View(FriendList);
+        }
+    }
+}
